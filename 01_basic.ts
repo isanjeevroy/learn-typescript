28253406fcb770ed5143700be4019dcdf2b65e9d
Sanjeev Kumar <sanjeevkumarcse19@gmail.com>
2026-07-06 01:26:10 +0530

    basic of ts

---
+++ 01_basic.ts
@@ -0,0 +1,8 @@
+function greet(name: string): string{
+    return `Hello ${name}`;
+}
+
+console.log(greet("Sanjeev"));
+
+//console.log(greet(24)); 
+//console.log(greet(24)); => This will also run and print 24 because, at the end runs javascript only.
--- basic.js
+++
@@ -1,6 +0,0 @@
-function greet(name){
-    return "Hello, ", name;
-}
-
-console.log(greet("hitesh"));
-console.log(greet(42));
