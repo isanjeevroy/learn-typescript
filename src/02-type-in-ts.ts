7a0fafb1ca9132c394e9a1f07bbb34a8c5eadb1a
Sanjeev Kumar <sanjeevkumarcse19@gmail.com>
2026-07-07 22:08:57 +0530

    types or data str in ts

---
+++ src/02-type-in-ts.ts
@@ -0,0 +1,16 @@
+let drink = "Chai";
+// this drink, we didn't give annotations as string but also it will understand that it's string if we will try to re-assign then it accept string only.
+
+// This called Inference
+
+let cups = Math.random() > 0.5 ? 10: 5;
+
+let bookName = "Lean Typescript";
+
+let authorName:string = "John Mone"
+// authorName = 2 this will give syntax error because it's annotation is string.
+// string
+// boolean 
+// integer
+
+// This called Annotations
