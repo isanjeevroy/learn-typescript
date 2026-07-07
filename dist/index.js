846c6a3778f0f398cd78871808a9fb0b9a077c9f
Sanjeev Kumar <sanjeevkumarcse19@gmail.com>
2026-07-07 22:10:43 +0530

    typescript

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(username) {
+    return `Hello ${username}`;
+}
+const name = "Sanjeev";
+console.log(greet(name));
+//# sourceMappingURL=index.js.map
